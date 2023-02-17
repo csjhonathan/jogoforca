2b75c66f01f40496e9e6a6f7deeef007ac7d1729
csjhonathan <jhonathancarv.s@gmail.com>
2023-02-17 18:04:18 -0300

    alterando data-test

--- src/components/Jogo/Palavra/Palavra.jsx
+++ src/components/Jogo/Palavra/Palavra.jsx
@@ -5,7 +5,6 @@ export default function Palavra({ palavraAtual, handleWord, endGame, img, correc
     <div className="rightBoard">
       <button data-test="choose-word" className="selectWord" onClick={() => handleWord()}>Escolher Palavra</button>
       <div data-test="word" className={`word ${endGame}`}>{img === 6 || endGame === "lose" ? palavraAtual : palavraAtual.map((e) => correct.includes(e) ? e : "_")}</div>
-      <div data-test="word" className={`word ${endGame}`}>{palavraAtual}</div>
     </div>
 
   )
